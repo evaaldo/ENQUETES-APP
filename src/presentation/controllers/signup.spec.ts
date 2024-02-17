898140e2ed7142dd48ba6a0c4dff598cc5d16d3a
evaldo <evaaldormj@gmail.com>
2024-02-16 22:47:09 -0300

    feat: add README.md

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -77,7 +77,7 @@ describe('SignUp Controller', () => {
     })
     test('Should return 400 if an invalid email is provided', () => {
         const { sut, emailValidatorStub } = makeSut()
-        jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false)
+        jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false )
         const httpRequest = {
             body: {
                 name: 'any_name',
